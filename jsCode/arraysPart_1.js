25c8b199a5e04b2390d6819b6735dba8f95b0527
prachiii30 <112465586+prachiii30@users.noreply.github.com>
2024-03-07 06:33:24 +0000

    files added

---
+++ jsCode/arraysPart_1.js
@@ -0,0 +1,26 @@
+const myArr=[1,2,3,4,5] //all types of datatypes cn be stored
+const myHeroes=["Hanuman","Shaktiman"];
+const arr=new Array(1,2,3,4);
+
+//n JavaScript, arrays aren't primitives but are instead Array objects 
+// resizable + mix of different data types
+
+//JavaScript array-copy operations create shallow copies. 
+//(All standard built-in copy operations with any JavaScript objects create shallow copies, rather than deep copies).
+//A shallow copy of an object is a copy whose properties share the same references  (changes are shown in original as well)
+//A deep copy of an object is a copy whose properties do not share the same references
+
+//Array Methods
+arr.push(-1);  //push() adds new element
+console.log(arr);
+
+arr.unshift(0);// add the element in the start
+console.log(arr);
+
+const ele=arr.shift()// removes the first element from the array and returns it
+console.log(ele);
+console.log(arr);
+
+const ele2=arr.pop(); // removes the last element from array
+console.log(ele2);
+console.log(arr);
