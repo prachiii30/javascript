0968cac0727eb63e1641b09642b0aa65cf41be3e
prachiii30 <112465586+prachiii30@users.noreply.github.com>
2024-03-12 14:29:32 +0000

    files Added

--- jsCode/objects_part1.js
+++ jsCode/objects_part1.js
@@ -3,13 +3,32 @@
 // Object.create
 
 //object literals
-
+const  s= Symbol("key1"); //important
 const user={
-    name:"Prachi"
+    name:"Prachi",
+    age:23,
+    email:"prachisingh30aabb@gmail.com",
+    location:"kanpur",
+    [s]:"Symbol keys value"
 }
 
 // can access in two ways
 // 1: user.name
 console.log(user.name)
-// 2: user[name]
-console.log(user[name]);
+// 2: user["name"] //better becz if in case we had defined any key as full name then with the help of first method we wont be able to access
+console.log(user["name"]);
+console.log(user[s]);  //for symobl no double quotes
+
+// Object.freeze(user);//no chnges further
+console.log(user);
+
+user.greeting=function(){
+    console.log("Hellow! How are you?")
+}
+console.log(user.greeting); //[Function (anonymous)] function not executed only reference
+console.log(user.greeting());
+
+user.greeting2=function(){
+    console.log(`hello!, ${this.name}`);
+}
+console.log(user.greeting2());
---
+++ jsCode/objects_part2.js
@@ -0,0 +1,14 @@
+const tinderuser=new Object();
+console.log(tinderuser);
+
+const regularuser={
+    email:"abc@gmail.com",
+    fullnanme:{
+        firstName:"tinder",
+        sirName:{
+            middleName:"abc",
+            lastName:"dhillon"
+        }
+    }
+}
+
