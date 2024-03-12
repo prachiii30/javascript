d06448575fe7d9cbc458e3b12c4ac0518bc200c7
prachiii30 <112465586+prachiii30@users.noreply.github.com>
2024-03-12 14:02:59 +0000

    files Added

---
+++ jsCode/objects_part1.js
@@ -0,0 +1,15 @@
+// singleton
+// whenever we make obj from constructor then we get singleton
+// Object.create
+
+//object literals
+
+const user={
+    name:"Prachi"
+}
+
+// can access in two ways
+// 1: user.name
+console.log(user.name)
+// 2: user[name]
+console.log(user[name]);
