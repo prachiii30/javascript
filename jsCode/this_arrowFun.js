bae43c1751f5be05b7391a4cb1dbdaf3312c5218
prachiii30 <112465586+prachiii30@users.noreply.github.com>
2024-03-14 11:17:47 +0000

    file added

--- jsCode/this_arrowFun.js
+++ jsCode/this_arrowFun.js
@@ -7,11 +7,38 @@ const user={
 
     welcomeMsg:function(){
         console.log(`${this.name},welcome to the code file`);
-        console.log(this)
+        console.log(this) //inside fun which was inside thw obj
     }
 }
 
 user.welcomeMsg()
 console.log(this)
 //inside windiw global object is window object
-//inside evn like node ,dino,etc the global object is {} i.e. empty object
+//inside env like node ,dino,etc the global object is {} i.e. empty object
+
+//printing the this inside function which is not inside the object
+function fun(){
+    console.log(this);
+}
+fun()
+
+function fun2(){
+    const usern="hanna"
+    console.log(this.usern); //giver undefined as the o/p 
+}
+fun2();
+
+// -----------------------------------------------
+
+const a=()=>{
+    console.log(this);  
+}
+a();
+
+//implicit,, whne we use {} then we use return when we use () we dont need return only for 1 liner
+
+const a1=(num1,num2)=> (num1+num2)
+console.log(a1(42,546));
+
+const objReturn = () => ({user:"Prachi Singh Parihar"})
+console.log(objReturn())
