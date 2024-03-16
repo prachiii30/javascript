bac89822d27273decb50ae2e10bf57134bf12588
prachiii30 <112465586+prachiii30@users.noreply.github.com>
2024-03-16 09:31:38 +0000

    files added

---
+++ jsCode/controlFlow/loops/Higher_order_array_loops.js
@@ -0,0 +1,49 @@
+//for of
+//mostly array specific loops
+
+
+//in arrays
+const arr=[8,2,3,4,5] 
+for(const index of arr){  //
+    console.log(index)
+}
+
+//Maps contains unique key value pair map it is an object
+
+//in maps
+const map=new Map()
+map.set('in','India')
+map.set('FR','France')
+map.set('usa','United states of America')
+console.log(map);
+
+for(const [key,value] of map){
+    console.log(key)
+}
+
+//in objects
+const obj={
+    'game1': 'NFS',
+    'game2' : 'Spiderman'
+}
+
+//we cannot over objects using for of loop
+
+const obj1={
+    js:"javascript",
+    cpp:"c++",
+    rb:"ruby"
+}
+
+for (const key in obj1){
+    console.log(key);
+    console.log(key, obj1[key])
+}
+
+//for in loop can be used in obj and arr 
+
+const arre=["prachi","sherya","shruti"]
+for( const i in arre ){
+    console.log(i);
+    console.log(arre[i])
+}
---
+++ jsCode/controlFlow/loops/iterations.js
@@ -0,0 +1,13 @@
+for(let i=0;i<10;i++ ){
+    console.log(i)
+}
+
+//break and continue
+
+for(i=0;i<20;i++){
+   
+    if(i==5)
+    // break;
+    continue;
+    console.log(i)
+}
