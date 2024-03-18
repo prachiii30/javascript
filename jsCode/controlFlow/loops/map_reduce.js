fe81a840dc6a924912d62a0d38c80729130ad697
prachiii30 <112465586+prachiii30@users.noreply.github.com>
2024-03-18 15:38:42 +0000

    files Added

--- jsCode/controlFlow/loops/map_reduce.js
+++ jsCode/controlFlow/loops/map_reduce.js
@@ -14,3 +14,20 @@ console.log(numss);
 const initial_val=0;
 const sum=mynums.reduce((acc,cu_val)=>acc+cu_val,initial_val)
 console.log(sum)
+
+//shopping cart
+const cart=[
+   { course:"js",
+    price:1000
+},
+{
+    course:"python",
+    price:100
+},
+{
+    course:"Java",
+    price:2000
+}
+]
+const sum1=cart.reduce((acc,ini)=>acc+ini.price,0)
+console.log(sum1);
