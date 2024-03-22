5391c6671e9d389bba6c3fe47832b30c391d634b
prachiii30 <112465586+prachiii30@users.noreply.github.com>
2024-03-22 16:12:52 +0000

    files Added

---
+++ Project/project4_guessTheNumber/index.js
@@ -0,0 +1,8 @@
+const random_num=parseInt((Math.random())*100+1)
+console.log(random_num)
+
+const submit_btn=document.querySelector('#subt')
+const input=document.querySelector('#guessField');
+const guess_array=document.querySelector('.gussess');
+const remaining_chance=document.querySelector('.lastResult');
+
