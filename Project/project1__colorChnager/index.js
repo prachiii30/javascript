e4240487300246f7326a91168c315616b430287b
prachiii30 <112465586+prachiii30@users.noreply.github.com>
2024-03-22 06:34:30 +0000

    files Added

--- Project/project1__colorChnager/index.js
+++ Project/project1__colorChnager/index.js
@@ -1,32 +1,58 @@
-const All_buttons=document.querySelectorAll('.button');
-console.log(All_buttons)
+const button=document.querySelectorAll('.button')
+console.log(button)
+button.forEach((btn)=>{
+btn.addEventListener('click',(ev)=>{
+    console.log(ev)
+    console.log(ev.target)
+    const id=ev.target.id
 
-const body=document.querySelector('body');
+    switch(id){
+        case "grey":
+            document.body.style.backgroundColor="grey"
+            break;
+        case "white":
+            document.body.style.backgroundColor="white"
+            break;
+        case "blue":
+            document.body.style.backgroundColor="blue"
+            break;
+        case "yellow":
+        document.body.style.backgroundColor="yellow"
+        break;
+    }
 
-All_buttons.forEach((btn)=>{
-    btn.addEventListener('click',(eve)=>{
-        console.log(eve);
-        console.log(eve.target);
-        const id=eve.target.id;
-        switch(id){
-            case "grey":
-                document.body.style.backgroundColor="grey"
-                break;
-                case "yellow":
-                    document.body.style.backgroundColor="yellow"
-                break;
-                case "white":
-                    document.body.style.backgroundColor="white"
-                break;
-                case "blue":
-                    document.body.style.backgroundColor="blue"
-                break;
+})
+})
 
-        }
-    })
+// const All_buttons=document.querySelectorAll('.button');
+// console.log(All_buttons)
+
+// const body=document.querySelector('body');
+
+// All_buttons.forEach((btn)=>{
+//     btn.addEventListener('click',(eve)=>{
+//         console.log(eve);
+//         console.log(eve.target);
+//         const id=eve.target.id;
+//         switch(id){
+//             case "grey":
+//                 document.body.style.backgroundColor="grey"
+//                 break;
+//             case "yellow":
+//                 document.body.style.backgroundColor="yellow"
+//                 break;
+//             case "white":
+//                 document.body.style.backgroundColor="white"
+//                 break;
+//             case "blue":
+//                 document.body.style.backgroundColor="blue"
+//                 break;
+//         }
+//     })
 
     
-})
+// })
+
 
 
 
