8bf03a7792c8badebc67a5db99c6ed5be385c7f3
prachiii30 <112465586+prachiii30@users.noreply.github.com>
2024-03-27 06:25:57 +0000

    files Added

--- Async_code/script.js
+++ Async_code/script.js
@@ -1,4 +1,14 @@
 //js is single threaded
 // its sequntially 
-// 
+//generate random color uisng hex value
+
+const random_color=function (){
+    const hex="0123456789ABCDEF"
+    let color="#"
+    for(let i=0;i<6;i++){
+        color+=hex[Math.floor(Math.random()*16)];
+    }
+    return color;
+};
+console.log(random_color)
 
